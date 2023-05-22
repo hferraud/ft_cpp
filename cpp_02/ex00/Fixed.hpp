b59077b9d62605b1b520d323e59a52cc1d853f2f
Hugo Ferraud <hferraud@MacBook-Pro-de-BDE.local>
2023-05-22 07:36:49 +0200

    add cpp02 ex00

---
+++ cpp_02/ex00/Fixed.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hferraud <hferraud@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 07:03:00 by hferraud          #+#    #+#             */
+/*   Updated: 2023/05/22 07:03:00 by hferraud         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed {
+
+public:
+	Fixed();
+	Fixed(Fixed &);
+	~Fixed();
+	Fixed operator= (const Fixed &);
+	int getRawBits(void);
+	void setRawBits(int);
+
+private:
+	int rawBits;
+
+	static const int fractional;
+};
+
+#endif
---
+++ cpp_02/ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hferraud <hferraud@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 07:03:00 by hferraud          #+#    #+#             */
+/*   Updated: 2023/05/22 07:03:00 by hferraud         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed () {
+	rawBits = 0;
+	fractional = 8;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed (Fixed &src) {
+	rawBits = src.getRawBits();
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed::~Fixed () {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed Fixed::operator= (const Fixed &src) {
+
+}
+
+int Fixed::getRawBits () {
+	return (rawBits);
+}
+
+void Fixed::setRawBits (int rawBits) {
+	this->rawBits = rawBits;
+}
