c84e41b7cf27d0fe1f7e71117d3aa428054ea283
mururoahh <ferraudhugo@gmail.com>
2023-06-10 09:28:36 +0200

    edit ex00 and add ex01

---
+++ cpp_03/ex01/ScavTrap.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mururoah <mururoah@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/10 08:53:00 by mururoah          #+#    #+#             */
+/*   Updated: 2023/06/10 08:53:00 by mururoah         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap() {}
+
+ScavTrap::ScavTrap(ScavTrap& const other) {}
+
+ScavTrap::~ScavTrap() {}
+
+ScavTrap& ScavTrap::operator=(ScavTrap& const other) {}
---
+++ cpp_03/ex01/ScavTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mururoah <mururoah@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/10 08:53:00 by mururoah          #+#    #+#             */
+/*   Updated: 2023/06/10 08:53:00 by mururoah         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_CPP_SCAVTRAP_HPP
+#define FT_CPP_SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap{
+
+public:
+	ScavTrap();
+
+	ScavTrap(ScavTrap& const other);
+
+	~ScavTrap();
+
+	ScavTrap& operator=(ScavTrap& const other);
+};
+
+
+#endif
